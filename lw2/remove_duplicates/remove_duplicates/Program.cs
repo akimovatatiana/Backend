41d37f5e2fba39d2d725a77da9d8211b40cecd76
Akimova Tatiana <akimova.tnya@gmail.com>
2020-03-18 13:49:24 +0300

    Added remove_duplicates

---
+++ lw2/remove_duplicates/remove_duplicates/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace remove_duplicates
+{
+    class Program
+    {
+        static string RemoveDuplicateChars(string str)
+        {
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                string symbol = str[i].ToString();
+                str = str.Replace(symbol, "");
+                str = str.Insert(i, symbol);
+            }
+            return str;
+        }
+        static void Main(string[] args)
+        {
+            string inputString = "";
+            if (args.Length == 1)
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    inputString += args[i];
+                }
+                string resString = RemoveDuplicateChars(inputString);
+                Console.WriteLine(resString);
+            }
+            else
+            {
+                Console.WriteLine("Incorrect number of arguments!" + "\n" + "Usage remove_duplicates.exe <input string>");
+            }
+        }
+    }
+}
