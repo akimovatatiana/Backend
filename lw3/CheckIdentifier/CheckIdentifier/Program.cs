9918e6d76e2746dcd5df9739ab7a84fee30aea93
Akimova Tatiana <akimova.tnya@gmail.com>
2020-03-28 10:26:03 +0300

    Added CheckIdentifier and CheckIdentifierTests

---
+++ lw3/CheckIdentifier/CheckIdentifier/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace CheckIdentifier
+{
+    public class Program
+    {
+        public const string NoMessage = "No";
+        public const string YesMessage = "Yes";
+        public const string InvalidArgumentsCount = "Incorrect number of arguments! Usage CheckIdentifier.exe <identifier>";
+        public const string EmptyStringMessage = "Identifier must not be an empty string";
+        public const string StartsWithDigitSymbolMessage = "Identifier must not start with a digit symbol";
+        public const string StartsWithNotLetterSymbolMessage = "Identifier must not start with a non-letter symbol";
+        public const string NotLetterOrNotDigitMessage = "Identifier must not have a non-letter or non-digit symbol";
+        public static bool CheckDigit(char ch)
+        {
+            if (!char.IsDigit(ch))
+            {
+                return false;
+            }
+            return true;
+        }
+        public static bool CheckLetter(char ch)
+        {
+            if (!char.IsLetter(ch))
+            {
+                return false;
+            }
+            return true;
+        }
+        public static string GetIdentifier(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                throw new Exception(InvalidArgumentsCount);
+            }
+            return args[0];
+        }
+        public static bool CheckIdentifierOnSR3(string identifier)
+        {
+            if (identifier == "")
+            {
+                Console.WriteLine(EmptyStringMessage);
+                return false;
+            }
+            if (CheckDigit(identifier[0]))
+            {
+                Console.WriteLine(StartsWithDigitSymbolMessage);
+                return false;
+            }
+            if (!CheckLetter(identifier[0]))
+            {
+                Console.WriteLine(StartsWithNotLetterSymbolMessage);
+                return false;
+            }
+            else
+            {
+                for (int i = 1; i < identifier.Length; i++)
+                {
+                    if (!CheckLetter(identifier[i]) && !CheckDigit(identifier[i]))
+                    {
+                        Console.WriteLine(NotLetterOrNotDigitMessage);
+                        return false;
+                    }
+                }
+                return true;
+            }
+        
+        }
+        public static void Main(string[] args)
+        {
+            try
+            {
+                string identifier = GetIdentifier(args);
+                if (CheckIdentifierOnSR3(identifier))
+                {
+                    Console.WriteLine(YesMessage);
+                }
+                else
+                {
+                    Console.WriteLine(NoMessage);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.GetType().Name}: " + InvalidArgumentsCount);
+            }
+
+        }
+    }
+}
---
+++ lw3/CheckIdentifier/CheckIdentifierTests/CheckIdentifierTests.cs
@@ -0,0 +1,102 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CheckIdentifierTests
+{
+    [TestClass]
+    public class CheckIdentifierTests
+    {
+        [TestMethod]
+        public void CheckDigit_WithNumber_ShouldReturnTrue()
+        {
+            char number = '1';
+            bool res = CheckIdentifier.Program.CheckDigit(number);
+            Assert.AreEqual(true, res);
+        }
+        [TestMethod]
+        public void CheckDigit_WithNotNumber_ShouldReturnFalse()
+        {
+            char notNumber = 'a';
+            bool res = CheckIdentifier.Program.CheckDigit(notNumber);
+            Assert.AreEqual(false, res);
+        }
+        [TestMethod]
+        public void CheckLetter_WithLetter_ShouldReturnTrue()
+        {
+            char letter = 'a';
+            bool res = CheckIdentifier.Program.CheckLetter(letter);
+            Assert.AreEqual(true, res);
+        }
+        [TestMethod]
+        public void CheckLetter_WithNotLetter_ShouldReturnFalse()
+        {
+            char notLetter = '5';
+            bool res = CheckIdentifier.Program.CheckLetter(notLetter);
+            Assert.AreEqual(false, res);
+        }
+        [TestMethod]
+        public void GetIdentifier_WithMoreThanOneArgument_ShouldThrowException()
+        {
+            string[] twoArgs =  { "ff", "dsd" };
+            try
+            {
+                CheckIdentifier.Program.GetIdentifier(twoArgs);
+            }
+            catch (System.Exception e)
+            {
+                Equals(e.Message, CheckIdentifier.Program.InvalidArgumentsCount);
+                return;
+            }
+            Assert.Fail("The expected exception was not thrown.");
+        }
+        [TestMethod]
+        public void GetIdentifier_WithNoArgument_ShouldThrowException()
+        {
+            string[] noArgs = { };
+            try
+            {
+                CheckIdentifier.Program.GetIdentifier(noArgs);
+            }
+            catch (System.Exception e)
+            {
+                Equals(e.Message, CheckIdentifier.Program.InvalidArgumentsCount);
+                return;
+            }
+            Assert.Fail("The expected exception was not thrown.");
+        }
+        [TestMethod]
+        public void CheckIdentifierOnSR3_WithStartsWithDigitIdentifier_ShouldReturnFalse()
+        {
+            string startsWithDigitIdentifier = "1hello";
+            bool result = CheckIdentifier.Program.CheckIdentifierOnSR3(startsWithDigitIdentifier);
+            Assert.AreEqual(false, result);
+        }
+        [TestMethod]
+        public void CheckIdentifierOnSR3_WithStartsWithNotLetterIdentifier_ShouldReturnFalse()
+        {
+            string startsWithNotLetterIdentifier = "-hello";
+            bool result = CheckIdentifier.Program.CheckIdentifierOnSR3(startsWithNotLetterIdentifier);
+            Assert.AreEqual(false, result);
+        }
+        [TestMethod]
+        public void CheckIdentifierOnSR3_WithEmptyIdentifier_ShouldReturnFalse()
+        {
+            string emptyStringIdentifier = "";
+            bool result = CheckIdentifier.Program.CheckIdentifierOnSR3(emptyStringIdentifier);
+            Assert.AreEqual(false, result);
+        }
+        [TestMethod]
+        public void CheckIdentifierOnSR3_WithNotDigitOrNotLetterIdentifier_ShouldReturnTrue()
+        {
+            string wrongIdentifier = "he_34g";
+            bool result = CheckIdentifier.Program.CheckIdentifierOnSR3(wrongIdentifier);
+            Assert.AreEqual(false, result);
+        }
+        [TestMethod]
+        public void CheckIdentifierOnSR3_WithRightIdentifier_ShouldReturnTrue()
+        {
+            string rightIdentifier = "hello";
+            bool result = CheckIdentifier.Program.CheckIdentifierOnSR3(rightIdentifier);
+            Assert.AreEqual(true, result);
+        }
+    }
+}
