8a50d1159a6c31094456716d4a29530b03e61c79
Akimova Tatiana <akimova.tnya@gmail.com>
2020-04-06 14:49:04 +0300

    Added Translator

---
+++ lw4/Translator/TranslatorTests/TranslatorTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Translator;
+
+namespace TranslatorTests
+{
+    [TestClass]
+    public class TranslatorTests
+    {
+        Translator.Translator dictionary = new Translator.Translator("../../../../Translator/dictionary.txt");
+        [TestMethod]
+        public void Translate_WithEmptyWord()
+        {
+            string word = "";
+            string result = dictionary.Translate(word);
+            Assert.AreEqual(null, result);
+        }
+        [TestMethod]
+        public void Translate_WithUnknownEnWord_ShouldReturnNull()
+        {
+            string word = "some";
+            string result = dictionary.Translate(word);
+            Assert.AreEqual(null, result);
+        }
+        [TestMethod]
+        public void Translate_WithUnknownRuWord_ShouldReturnNull()
+        {
+            string word = "еда";
+            string result = dictionary.Translate(word);
+            Assert.AreEqual(null, result);
+        }
+        [TestMethod]
+        public void Translate_WithRuWord_ShouldReturnEnTranslation()
+        {
+            string word = "собака";
+            string translation = dictionary.Translate(word);
+            Assert.AreEqual("dog", translation);
+        }
+        [TestMethod]
+        public void Translate_WithEnWord_ShouldReturnRuTranslation()
+        {
+            string word = "dog";
+            string translation = dictionary.Translate(word);
+            Assert.AreEqual("собака", translation);
+        }
+    }
+}
